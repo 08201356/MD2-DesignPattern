a64df22302e07c39f1d8593d9d43796802cb9785
08201356 <nguyenhoainma0613@gmail.com>
2023-09-06 17:24:44 +0700

    add work

---
+++ object-pool-example/src/MathCalculator.java
@@ -0,0 +1,15 @@
+public class MathCalculator implements Calculator {
+    @Override
+    public double add(double first, double second){
+        return first + second;
+    }
+    public double sub(double first, double second){
+        return first - second;
+    }
+    public double mul(double first, double second){
+        return first * second;
+    }
+    public double div(double first, double second){
+        return first / second;
+    }
+}
